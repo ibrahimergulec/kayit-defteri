383703adee91c159dde944666fb4c703ecc46c54
ibrahimergulec <85809244+ibrahimergulec@users.noreply.github.com>
2026-07-01 02:18:58 +0300

    Add service worker for caching static assets

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'kayit-defteri-v1';
+const STATIC_ASSETS = ['/', '/index.html', '/manifest.json'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(STATIC_ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => Promise.all(cacheNames.filter(name => name !== CACHE_NAME).map(name => caches.delete(name)))).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request))
+  );
+});
